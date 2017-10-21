246f954f954d5c1818fb43b0b6b9d788d2172d7c
nilsgeiselhart <nigeiselh@gmail.com>
2017-10-21 20:15:30 +0200

    edit build scripts

--- build_run_amd.sh
+++ build_run_amd.sh
@@ -3,7 +3,7 @@
 echo "BUILDING for AMD64 architecture"
 
 echo "--- building docker image... ---"
-docker build --build-arg BASE_IMG=node:alpine --tag nilsg/raspi-node-rest .
+docker build --build-arg BASE_IMG=node:alpine --tag nilsg/node-rest .
 
 echo "--- running docker container... ---"
-docker run -it --rm -p 8080:8080 -p 8443:8443 nilsg/raspi-node-rest ./entrypoint.sh
+docker run -it --rm -p 8080:8080 -p 8443:8443 nilsg/node-rest ./entrypoint.sh
--- build_run_arm.sh
+++ build_run_arm.sh
@@ -6,4 +6,4 @@ echo "--- building docker image... ---"
 sudo docker build --build-arg BASE_IMG=resin/raspberry-pi-alpine-node --tag nilsg/raspi-node-rest .
 
 echo "--- running docker container... ---"
-sudo docker run -it --rm -p 8080:8080 -p 8443:8443 nilsg/raspi-node-rest ./entrypoint.sh
+sudo docker run -it --rm -p 8080:8080 -p 8443:8443 nilsg/raspi-node-rest -v /dev/mem:/dev/mem -v /dev/gpiomem:/dev/gpiomem ./entrypoint.sh
