e4a7dfc096c19ff71801ff3aca9411e57b33d04b
nilsgeiselhart <nigeiselh@gmail.com>
2017-10-22 11:30:46 +0200

    build script

--- build_run_arm.sh
+++ build_run_arm.sh
@@ -6,4 +6,4 @@ echo "--- building docker image... ---"
 sudo docker build --build-arg BASE_IMG=resin/raspberry-pi-node:8.0.0 --tag nilsg/raspi-node-rest .
 
 echo "--- running docker container... ---"
-sudo docker run -it --rm -p 8080:8080 -p 8443:8443 --volume /dev/mem:/dev/mem --volume /dev/gpiomem:/dev/gpiomem nilsg/raspi-node-rest ./entrypoint_bash.sh
+sudo docker run -it --rm -p 8080:8080 -p 8443:8443 --cap-add SYS_RAWIO --device /dev/mem:/dev/mem --device /dev/gpiomem:/dev/gpiomem nilsg/raspi-node-rest ./entrypoint_bash.sh
