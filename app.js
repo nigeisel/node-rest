8a221a318a93985815d321904abdd7978147e242
nilsgeiselhart <nigeiselh@gmail.com>
2017-10-22 23:09:56 +0200

    activate cors

--- app.js
+++ app.js
@@ -15,11 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// TODO: Hangs.. why?
-//app.use(cors);
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,6 +27,7 @@ app.use('/api', api);
 
 app.use('/', base);
 
+app.use(cors);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
